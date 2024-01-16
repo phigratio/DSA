b5ff37aa448bf99edfba4d9db04de857742ab774
Muqtadir <muqtadir.mujahid02@gmail.com>
2024-01-17 00:29:08 +0600

    did the second task from the contest

--- Algorithms Course/lab1/contestTask2.cpp
+++ Algorithms Course/lab1/contestTask2.cpp
@@ -1,26 +1,59 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
+
+int countInversions(const string &s, int n)
+{
+  int inversion = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (s[i] == 'A')
+    {
+      continue;
+    }
+    for (int j = i + 1; j < n; j++)
+    {
+      if (s[i] > s[j])
+      {
+        inversion++;
+      }
+    }
+  }
+  return inversion;
+}
+
+bool compare(const pair<string, int> &a, const pair<string, int> &b)
+{
+  return a.second < b.second;
+}
+
 int main()
 {
   ll t;
   cin >> t;
+
   while (t--)
   {
     ll n, m;
     cin >> n >> m;
-    int ta = 19, tc = 17, tg = 13;
-    int gc = 4, ga = 6;
-    int ca = 2;
+
+    vector<pair<string, int>> strings;
 
     for (int i = 0; i < m; i++)
     {
-      string s;
-      cin >> s;
-      int count[m] = {0};
-      for (int j = 0; j < n; j++)
-      {
-      }
+      string current_string;
+      cin >> current_string;
+      int inversion = countInversions(current_string, n);
+      strings.push_back({current_string, inversion});
+    }
+
+    sort(strings.begin(), strings.end(), compare);
+
+    for (const auto &pair : strings)
+    {
+      cout << pair.first << endl;
     }
   }
+
+  return 0;
 }
